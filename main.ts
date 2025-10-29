48c3b073b6999db83188393b307114083a944df3
salamancafaiber64-sudo <salamancafaiber64@gmail.com>
2025-10-29 16:07:25 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("atras")
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("izquierda")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("adelante")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("derecha")
+})
+radio.setGroup(1)
+// Si no se presiona nada (detenerse)
 basic.forever(function () {
-	
+    radio.sendString("stop")
+    basic.pause(500)
 })
